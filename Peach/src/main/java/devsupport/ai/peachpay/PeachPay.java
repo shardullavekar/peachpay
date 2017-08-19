3d7c33109e4bc54169a9ead608e75f8feb76d1e9
Shardul <developershardul@gmail.com>
2017-08-19 17:51:48 +0530

    Added binders and other code snippets

--- Peach/src/main/java/devsupport/ai/peachpay/PeachPay.java
+++ Peach/src/main/java/devsupport/ai/peachpay/PeachPay.java
@@ -1,18 +1,38 @@
 package devsupport.ai.peachpay;
 
 import android.app.ProgressDialog;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 
+import com.oppwa.mobile.connect.checkout.dialog.CheckoutActivity;
+import com.oppwa.mobile.connect.checkout.meta.CheckoutSettings;
+import com.oppwa.mobile.connect.exception.PaymentError;
+import com.oppwa.mobile.connect.exception.PaymentException;
+import com.oppwa.mobile.connect.provider.Connect;
+import com.oppwa.mobile.connect.provider.Transaction;
+import com.oppwa.mobile.connect.provider.TransactionType;
+import com.oppwa.mobile.connect.service.ConnectService;
+import com.oppwa.mobile.connect.service.IProviderBinder;
+
 import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import devsupport.ai.peachpay.REST.CheckoutId;
 
 public class PeachPay extends AppCompatActivity {
+    private IProviderBinder binder;
+
+    private ServiceConnection serviceConnection;
+
     String server_url,
             amount, currency, type, env;
 
@@ -21,6 +41,21 @@ public class PeachPay extends AppCompatActivity {
     Bundle bundle;
 
     private ProgressDialog dialog;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, ConnectService.class);
+        startService(intent);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(serviceConnection);
+        stopService(new Intent(this, ConnectService.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +84,39 @@ public class PeachPay extends AppCompatActivity {
         server_url = bundle.getString(Config.SERVER_URL);
 
         if (checkValidation()) {
+            initEnv();
             setContentView(R.layout.activity_main);
             getCheckoutId();
         }
+    }
+
+    private void initEnv() {
+        serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                binder = (IProviderBinder) service;
+
+                try {
+                    if (TextUtils.equals(env, Config.TEST)) {
+                        binder.initializeProvider(Connect.ProviderMode.TEST);
+                    }
+                    else if (TextUtils.equals(env, Config.TEST)){
+                        binder.initializeProvider(Connect.ProviderMode.LIVE);
+                    }
+                    else {
+                        endActivity(Config.FAILED, "Invalid Environment");
+                    }
+                } catch (PaymentException ee) {
+	                endActivity(Config.FAILED, ee.getMessage());
+                }
+
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                binder = null;
+            }
+        };
     }
 
     private void getCheckoutId() {
@@ -62,6 +127,21 @@ public class PeachPay extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void configCheckout(String checkoutId) {
+        Set<String> paymentBrands = new LinkedHashSet<String>();
+
+        paymentBrands.add("VISA");
+        paymentBrands.add("MASTER");
+        paymentBrands.add("DIRECTDEBIT_SEPA");
+
+        CheckoutSettings checkoutSettings = new CheckoutSettings(checkoutId, paymentBrands);
+
+        Intent intent = new Intent(PeachPay.this, CheckoutActivity.class);
+        intent.putExtra(CheckoutActivity.CHECKOUT_SETTINGS, checkoutSettings);
+
+        startActivityForResult(intent, CheckoutActivity.CHECKOUT_ACTIVITY);
     }
 
     private boolean checkValidation() {
@@ -91,6 +171,35 @@ public class PeachPay extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (resultCode) {
+            case CheckoutActivity.RESULT_OK:
+            /* transaction completed */
+                Transaction transaction = data.getParcelableExtra(CheckoutActivity.CHECKOUT_RESULT_TRANSACTION);
+
+            /* resource path if needed */
+                String resourcePath = data.getStringExtra(CheckoutActivity.CHECKOUT_RESULT_RESOURCE_PATH);
+
+                if (transaction.getTransactionType() == TransactionType.SYNC) {
+                /* check the result of synchronous transaction */
+                } else {
+                /* wait for the asynchronous transaction callback in the onNewIntent() */
+                }
+
+                break;
+            case CheckoutActivity.RESULT_CANCELED:
+                endActivity(Config.FAILED, "Shoper cancelled transaction");
+                break;
+            case CheckoutActivity.RESULT_ERROR:
+                PaymentError error = data.getParcelableExtra(CheckoutActivity.CHECKOUT_RESULT_ERROR);
+                endActivity(Config.FAILED, error.getErrorMessage());
+                break;
+            default:
+                break;
+        }
     }
 
     private void endActivity(int resultCode, String message) {
